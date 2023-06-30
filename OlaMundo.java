bf2c8cd821ad68f2330b9ffaaa60b55f96ebbbc8
M. César Brancalhão <58019405+cesarbrancalhao@users.noreply.github.com>
2023-06-30 12:26:26 -0300

    Add files via upload

---
+++ OlaMundo.java
@@ -0,0 +1,6 @@
+public class OlaMundo {
+    public static void main(String[] args){
+        System.out.println("Olá mundo.");
+        System.out.println("Como vocês estão?");
+    }
+}
---
+++ entrada.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class entrada {
+    public static void main(String[] args){
+        System.out.println("Escreva a ");
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}

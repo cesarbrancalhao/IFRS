979c795b0acdb40f7f7db1da749a075aed0dcf82
M. César Brancalhão <58019405+cesarbrancalhao@users.noreply.github.com>
2023-07-02 01:37:36 -0300

    Create exercicio1_2.java

---
+++ Java II/lista 2/exercicio1_2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class exercicio1_2 {
+    public static void main(String[] args) {
+        double numero = 0, soma = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < 4; i++){
+            System.out.printf("Insira o número %d: ", i+1);
+            numero = scanner.nextDouble();
+            soma = soma + numero;
+        }
+        scanner.close();
+
+        System.out.printf("%.1f", soma);
+    }
+}

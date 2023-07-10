ff594137e4792bb128353e8a07e963a5dfd54293
M. César Brancalhão <58019405+cesarbrancalhao@users.noreply.github.com>
2023-07-10 04:49:32 -0300

    Add files via upload

--- Java III/teste.java
+++ Java III/teste.java
@@ -1,25 +1,25 @@
-package j3;
+package POO;
 
-import java.util.Scanner;
 public class teste {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double A, B, C;
-        System.out.print("Medida A: ");
-        A = scanner.nextDouble();
-        System.out.print("Medida B: ");
-        B = scanner.nextDouble();
-        System.out.print("Medida C: ");
-        C = scanner.nextDouble();
-        if(A + B > C && A + C > B && B + C > A) {
-            if(A == B && B == C)
-                System.out.println("As medidas formam um triângulo EQUILÁTERO");
-            else if(A != B && A != C && B != C)
-                System.out.println("As medidas formam um triângulo ESCALENO");
-            else
-                System.out.println("As medidas formam um triângulo ISÓSCELES");
-        } else {
-            System.out.println("As medidas informadas NÃO formam um triângulo");
-        }
+        char model;
+        char color;
+        double tip;
+        int load;
+        boolean capped = false;
+
+        doodle(capped);
+        cap(capped);
+    }
+
+    public static void doodle(boolean a) {
+        if (a == false)
+            System.out.println("Rabisco");
+        else{System.out.println("Tampada");}
+    }
+    public static boolean cap(boolean a) {
+        if (a == false)
+            a = true;
+        return a;
     }
 }
